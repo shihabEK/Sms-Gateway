038fbb5b6e3de1868e328088a6ebfbade9572cd4
Shihab EK <ShihabEK@Shihabs-MacBook-Pro.local>
2019-07-11 23:06:05 +0530

    0.0.2
    * Auto Discovery added

--- src/config/sms.php
+++ src/config/sms.php
@@ -2,9 +2,16 @@
 
 
 return [
+
+    //MSG91 Parameters
+    //Log on to msg91.com for account
+
     'default_gateway' => 'MSG91',     // Available Gateways : MSG91
     'sender_id' => 'SKYKRU',     // 6 Characters only
-    'authKey'   => '141154Acpa1W8XVq58a15ead',
-    'default_route' => '4',
+    'authKey'   => '141154Acpa1W8XVq58a15ead', //authKey from MSG91
+    'default_route' => '4', // 1: Promotional 4: Transectional
+
+    //-------------------------
+
 
 ];
