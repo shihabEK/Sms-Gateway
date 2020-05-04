cd335e1faea0e5f7d0a964c303fd7ccb5b345fe4
Shihab EK <sekonlive@gmail.com>
2020-05-04 22:48:37 +0530

    Update CanBeSMS.php

--- src/Traits/CanBeSMS.php
+++ src/Traits/CanBeSMS.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the jcc/laravel-vote.
- *
- * (c) jcc <changejian@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 namespace shaab\sms\Traits;
 
 use shaab\sms\Facades\SMS;
